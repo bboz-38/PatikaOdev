33d7c5cd79506fae68e0f8210fab846db36b1f97
Burhan Boz <burhanboz38@gmail.com>
2024-11-22 11:04:10 +0300

    vücut kitle indeksini hesaplayan program yazıldı

---
+++ hello/src/Vucut.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Vucut {
+    public static void main(String[] args) {
+        double boy, kilo;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = inp.nextDouble();
+        double indeks = kilo / (boy * boy);
+        System.out.print("Vücut kitle indeksiniz: " + indeks);
+
+    }
+}
