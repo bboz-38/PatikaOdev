9caca51f7374e0c8ddec5c7e5091022e7db1c917
Burhan Boz <burhanboz38@gmail.com>
2024-11-22 10:39:04 +0300

    taksimetre hesaplayn program yazıldı

---
+++ hello/src/taksi.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class taksi {
+    public static void main(String[] args) {
+        int km;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi km cinsinden giriniz: ");
+        km = input.nextInt();
+        double kmUcreti = 2.20;
+        int minimumTutar = 20;
+        int acilisUcreti = 10;
+        double odenecekTutar = (km * kmUcreti) + acilisUcreti;
+
+        if (odenecekTutar < 20) {
+            odenecekTutar = minimumTutar;
+            System.out.println("Borcunuz: " + minimumTutar);
+        } else {
+            System.out.println("Borcunuz: " + odenecekTutar);
+        }
+    }
+}
